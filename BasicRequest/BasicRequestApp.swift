4c6aea70d46344e2639a054a2519add77dbeaed8
Dmitrii <dimaeselidze@icloud.com>
2024-05-10 15:26:52 +0300

    Initial Commit

---
+++ BasicRequest/BasicRequestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BasicRequestApp.swift
+//  BasicRequest
+//
+//  Created by Dmitrii Eselidze on 10.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct BasicRequestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
